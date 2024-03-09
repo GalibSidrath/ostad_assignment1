4826abe8274151d0c4f811bed3148809a198dd1e
Abd M Galib <abdmgalib2001@gmail.com>
2024-03-09 21:21:32 +0600

    Initial commit

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'singing.dart';
+
+void main(){
+  Media media_object = Media();
+  media_object.play();
+
+  Song song_object = Song('No Singer');
+  song_object.play();
+}
---
+++ singing.dart
@@ -0,0 +1,17 @@
+class Media{
+
+  void play(){
+    print('Playing media...');
+  }
+}
+
+class Song extends Media{
+  String artist;
+
+  Song(this.artist){}
+
+  @override
+  void play() {
+    print('Playing song by $artist');
+  }
+}
